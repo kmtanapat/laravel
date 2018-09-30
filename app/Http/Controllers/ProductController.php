371cf269affc1f97ca03948dd0c7bf97b13cda3e
Orm Wiwitumpon <cutiemoon08@yahoo.com>
2018-09-30 15:15:20 +0700

    edit nav

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
-class StatusController extends Controller
+class ProductController extends Controller
 {
   public function index(){
     $sql = "SELECT s.statusId, s.statusName, count(c.statusId) as 'amount', s.description from status s left join candidates c on c.statusId = s.statusId group by s.statusId";
