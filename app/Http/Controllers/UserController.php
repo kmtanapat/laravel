40153174da80229b3f4972d567d83e1eb2f2371e
Orm Wiwitumpon <cutiemoon08@yahoo.com>
2018-09-30 13:17:06 +0700

    edit user page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
-class AppointmentController extends Controller
+class UserController extends Controller
 {
   public function index(){
     //$sql = "SELECT s.statusId, s.statusName, count(c.statusId) as 'amount', s.description from status s left join candidates c on c.statusId = s.statusId group by s.statusId";
-    $apm = DB::table('appointmentstype')->get();
-    return view('appointmentstype', ['apm' => $apm]);
+//    $apm = DB::table('appointmentstype')->get();
+    return view('user');
   }
 
   public function create(){
