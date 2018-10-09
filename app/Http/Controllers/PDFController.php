b078631b0d013c25a822fbcb4d1ca19eea53e6d5
kmtanapat <tanapat.k@arms-asia.com>
2018-10-09 13:46:58 +0700

    esus

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Order;
+use PDF;
 
 class PDFController extends Controller
 {
-    //
+    public function pdf()
+    {
+        $Orders = Order::all();
+        $pdf = PDF::loadview('pdf',['Orders' =>$Orders]);
+        return @$pdf->stream();
+    }
 }
